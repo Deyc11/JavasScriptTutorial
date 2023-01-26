6ab71c47de2b11b1eb3ad514592ee906ade69365
Amadej <blazincic.amadej@gmail.com>
2023-01-26 14:03:47 +0100

    Java Script tutorial

---
+++ main.js
@@ -0,0 +1,162 @@
+// // Log a statement using console.log()
+// console.log('hello from main.js')
+
+// //Variables
+
+// let age = 25
+// console.log(age)
+
+// // const salary = 80000
+// // console.log(salary)
+// //const(se ne spreminja) in let(se lahko spreminja) je za stevilke.
+
+// let sum = 0
+// sum = 5
+// console.log(sum)
+
+
+// //string
+// const name = 'Amadej'
+// const language = "JavaScript"
+// //number
+// const total = 0
+// const PI = 3.14
+// //boolean
+// const isPrimaryNumber = true
+// const isNewUser = false
+// //
+// let result
+// console.log(result)
+
+// const res = undefined
+// //null type
+// const data = null
+
+// //one way of storing the data
+// const person = {
+//     'firstName' : 'Bruce',
+//     'lastName:': 'Wyne',
+//     'age': 30
+// }
+
+// console.log(person.firstName)
+
+// //polje se začne z 0
+// const oddNumbers = [1, 3, 5, 7, 9]
+// console.log(oddNumbers[1])
+
+// let a = 10
+// a = 'Amadej'
+// a = true
+// console.log(a)
+
+// //assignment operator
+// let x = 10
+// let y = 5
+
+// //aritmetic operators
+// console.log(x%y)
+// console.log(++x)
+// console.log(--y)
+
+// //boolean operators
+// console.log(x<=y)
+
+// const isValidNumberAnd = x > 8 && 8 > y
+// const isValidNumberPipe = x > 8 || 8 > y
+// console.log(isValidNumber)
+
+
+// const var1 ='test'
+// const var2 ='test'
+
+// console.log(var1 == var2)
+// console.log(var1 === var2)
+
+
+//IF, IF ELSE, ELSE
+// const num = 0
+
+// if(num > 0) {
+//     console.log('Number is positive')
+// }
+// else if(num < 0){
+//     console.log('Number is negative')
+
+// }
+// else{
+//     console.log('Number is zero')
+// }
+
+
+//SWITCH
+// const color = 'brown'
+
+// switch(color){
+//     case 'red':
+//         console.log('Color is red')
+//         break
+//     case 'blue':
+//         console.log('Color is blue')
+//         break
+//     case 'green':
+//         console.log('Color is green')
+//         break
+//         default:
+//             console.log('Not a valid color')
+// }
+
+
+//FOR LOOP
+// for(let i = 1; i <=5; i++){
+//     console.log('Iteration number ' + i)
+// }
+
+
+//WHILE LOOP
+//  let i = 1
+//  while(i <= 5){
+//      console.log('Iteration number' +i)
+//      i++
+//  }
+
+
+//DO WHILE...is executed at least once
+// let i = 1
+// do{
+//     console.log('Iteration number' +i)
+//     i++
+// }while(i<=5)
+
+
+//best for collections of data
+//you dont have to keep track of value
+// const numArray = [1, 2, 3, 4, 5]
+// for(const num of numArray){
+//     console.log('Number ' + num)
+// }
+
+
+//FUNCTIONS
+//v oklepaju je parameter
+// function greet(username){
+//     console.log('Dobro jutro' + username)
+// }
+// greet('Buruce')
+// greet('Amadej')
+// greet('Timea')
+
+// function arrowSum(a, b){
+//     return a + b 
+// }
+// const arrowSum = () => {
+//     return a + b
+// }
+// const sum = arrowSum(5, 10)
+// console.log(sum)
+
+if(true){
+    const myName = 'Amadej'
+    console.log(myName)
+}
+
